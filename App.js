1c155b1efd68e0e3db0e9d712655a57d41b5044c
angelakrw <krewski.angela@gmail.com>
2024-02-20 21:47:07 -0300

    Limpieza de imports innecesarios

--- App.js
+++ App.js
@@ -1,15 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Home from './src/Screens/Home';
-import { useState } from 'react';
-import ItemListCategories from './src/Screens/ItemListCategories';
+import { StyleSheet } from 'react-native';
 import { useFonts } from 'expo-font';
 import { fonts } from './src/Global/fonts';
 import Navigator from './src/Navigation/Navigator';
 
 export default function App() {
 
-  const [categorySelected, setCategorySelected] = useState('')  
   const [fontsLoaded] = useFonts(fonts)
 
   if (!fontsLoaded) {
